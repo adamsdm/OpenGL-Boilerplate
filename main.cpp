6efdebbfb35f298d225cc4147a868c13691b8098
Adam <adamsdm1@gmail.com>
2018-01-29 18:02:17 +0100

    Added uniform to shader program

--- main.cpp
+++ main.cpp
@@ -167,6 +167,8 @@ int main() {
 	// *********** Main Loop *********** //
 	// ********************************* //
 	
+	float time = glfwGetTime();
+
 	while (!glfwWindowShouldClose(window))
 	{
 		// Check for user inputs
@@ -178,10 +180,14 @@ int main() {
 
 		// draw our first triangle
 		glUseProgram(shaderProgram);
+
+		time = glfwGetTime();
+		int vertexUniformLocation = glGetUniformLocation(shaderProgram, "time");
+		glUniform1f(vertexUniformLocation, time);
+		
+
 		glBindVertexArray(VAO); // seeing as we only have a single VAO there's no need to bind it every time, but we'll do so to keep things a bit more organized
-								//glDrawArrays(GL_TRIANGLES, 0, 6);
 		glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
-		// glBindVertexArray(0); // no need to unbind it every tim
 
 
 		glfwSwapBuffers(window);
